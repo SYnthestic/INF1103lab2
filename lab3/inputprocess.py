af7c75823f5764b63f73fb566564c8e799a87d56
Samuel Yam <2603176@sit.singaporetech.edu.sg>
2026-09-21 17:26:12 +0800

    Deleted attempted Class OOP attempt at Lab 3

--- lab3/inputprocess.py
+++
@@ -1,9 +0,0 @@
-class InputProcessor:
-    def __init__(self):
-        self.entry_inp = "0"
-        self.inventory_entry = []
-        self.failed_entries = 0
-
-    def input_num(self):
-        self.entry_inp = input("Enter a number between 0 and 500: ")
-        return self.entry_inp
